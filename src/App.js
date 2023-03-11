70ce26dbcf334b15556fe3dd5787a3565ced1cb7
arifbilir <arifbilirde@gmail.com>
2023-03-11 17:37:29 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
-  return (
+    return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>
   );
 };
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Main = () => {
+    return(
+        <div>
+            <Navbar/>
+            <Main/>
+        </div>
+    )
+}
+
+export default Main
